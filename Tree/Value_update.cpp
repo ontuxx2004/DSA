b40fe983f5e5511f8ab7c8b9bd2a595cb6379a9d
Ontu Boss <your@email.comonemanarmysavage69@gmail.com>
2026-09-02 02:18:52 +0600

    Add segment tree value update

---
+++ Tree/Value_update.cpp
@@ -0,0 +1,129 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector <int> tree;
+void buildTree(int node,int left,int right,vector <int>&arr)
+{
+     if(left==right)
+     {
+        tree[node]=arr[left];
+        return;
+     }
+
+     int mid=(left+right)/2;
+
+     buildTree(2*node+1,left,mid,arr);
+     buildTree(2*node+2,mid+1,right,arr);
+     tree[node]=tree[2*node+1]+tree[2*node+2];
+}
+
+int query(int node,int left,int right,int ql,int qr)
+{
+    // no overlap
+    if(right<ql || left>qr )
+    {
+        return 0;
+    }
+
+    //complete overlap
+
+    if(left>=ql && right <=qr)
+    {
+        return tree[node];
+    }
+
+    //partial overlap
+
+    int mid=(left+right)/2;
+    int leftTree=query(2*node+1,left,mid,ql,qr);
+    int rightTree=query(2*node+2,mid+1,right,ql,qr);
+    return leftTree+rightTree;
+}
+
+
+    //update function 
+
+    void update( int node,int left, int right,int index,int value)
+    {
+        if(left==right)
+        {
+            tree[node]=value;
+            return ;
+        
+        }
+
+        int mid=(left+right)/2;
+
+        if(index<=mid)
+        {
+            // thn go to left
+
+            update(2*node+1,left,mid,index,value);
+        }
+
+        else
+        {
+            //thn go to right 
+
+            update(2*node+2,mid+1,right,index,value);
+        }
+        
+        tree[node]=tree[2*node+1]+tree[2*node+2];
+    }
+
+int main()
+{
+    int n;
+    cout<<"enter arry size =";
+    cin>> n;
+
+    vector<int>arr(n);
+
+    cout<<"enter arry elements :"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    //segment tree resize
+    tree.resize(4*n);
+
+    //build tree
+
+    buildTree(0,0,n-1,arr);
+
+    //query input
+
+    int ql,qr;
+    cout<<"enter query range :"<<endl;
+    cin>>ql>>qr;
+
+    //output
+
+    cout<<"sum ="<<query(0,0,n-1,ql,qr)<<endl;
+
+    //update 
+
+    int index,value;
+
+    cout<<"enter index to update: "<<endl;
+    cin>>index;
+
+    cout<<"enter value :"<<endl;
+    cin>>value;
+
+    update(0,0,n-1,index,value);
+
+    //sum after update
+
+    cout << "enter query range after update :" << endl;
+    cin >> ql >> qr;
+
+    cout << "sum after update = "
+         << query(0, 0, n-1, ql, qr) << endl;
+
+
+
+    return 0;
+
+
+}
